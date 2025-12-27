7e3a1a33d8b3db382b9a42e40efdfc3214721a3a
neonnskye <theneonnofficial@gmail.com>
2025-12-27 19:59:31 +0530

    Add script config

---
+++ config.py
@@ -0,0 +1,8 @@
+IMAGES_CAPTIONS = {
+    "images/image1.jpg": "Mui..♡",
+    "images/image2.jpg": "🩶⌨️🎧💿",
+    "images/image3.jpg": "Alternative elf",
+    "images/image4.jpg": "💫",
+    "images/image5.jpg": "Fashion inspo - crochet maxi skirt",
+    "images/image6.jpg": "death by thrifting",
+}
